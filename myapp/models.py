968efc31b2581759d35247eec8244ef963e7e518
Ashutosh <prajapatiaashu4444@gmail.com>
2023-05-23 13:00:21 +0530

    Updated 'Label' model to fix JSONDecode error.

--- myapp/models.py
+++ myapp/models.py
@@ -17,15 +17,15 @@ class Note(models.Model):
 
 class Label(models.Model):
     id = models.AutoField
-    note_title = models.CharField(max_length=200, default="", blank=True)
-    note = models.CharField(max_length=500, default="", blank=True)
+    note_title = models.CharField(max_length=200, default=list, blank=True)
+    note = models.CharField(max_length=500, default=list, blank=True)
     add_date = models.DateField(default="")
     author = models.CharField(max_length=200, default="")
     password = models.CharField(max_length=200, default="", blank=True)
-    note_title_when_locked = models.CharField(max_length=500, default="", blank=True)
-    note_when_locked = models.CharField(max_length=500, default="", blank=True)
+    note_title_when_locked = models.CharField(max_length=500, default=list, blank=True)
+    note_when_locked = models.CharField(max_length=500, default=list, blank=True)
     label = models.CharField(max_length=200, default="")
-    note_id = models.CharField(max_length=200, default="", blank=True)
+    note_id = models.CharField(max_length=200, default=list, blank=True)
 
     def __str__(self):
         return self.label
